1a15be424e8cb595101c5c9db0e96b5cf9d75054
Нестерова Таисия Григорьевна <tayanesterova91@gmail.com>
2023-11-11 20:09:02 +0700

    binding form with data

--- form/src/components/Form.vue
+++ form/src/components/Form.vue
@@ -3,34 +3,34 @@
         <div class="section">
             <div class="row">
                 <label class="label">Фамилия</label>
-                <input type="text" class="input-text" required/> 
+                <input type="text" class="input-text" required v-model="client.surname" placeholder="Фамилия"/> 
             </div>
             <div class="row">
                 <label class="label">Имя</label>
-                <input type="text" class="input-text" required/> 
+                <input type="text" class="input-text" required v-model="client.name" placeholder="Имя"/> 
             </div>
             <div class="row">
                 <label class="label">Отчество</label>
-                <input type="text" class="input-text" /> 
+                <input type="text" class="input-text" v-model="client.secondName" placeholder="Отчество"/> 
             </div>
             <div class="row">
                 <label class="label">Дата рождения</label>
-                <input type="text" class="input-text" required/> 
+                <input type="text" class="input-text" required v-model="client.birthDate" placeholder="ДД.ММ.ГГГГ"/> 
             </div>
             <div class="row">
                 <label class="label">Номер телефона</label>
-                <input type="phone" class="input-text" required/> 
+                <input type="phone" class="input-text" required v-model="client.phone" placeholder="7XXXXXXXXXX"/> 
             </div>
             <div class="row">
                 <label class="label">Пол</label>
-                <input type="radio" name="gender" value="male" class="input-radio"/>
+                <input type="radio" name="gender" value="male" class="input-radio" v-model="client.gender"/>
                 <label class="label">Мужской</label>
-                <input type="radio" name="gender" value="female" class="input-radio" />
+                <input type="radio" name="gender" value="female" class="input-radio" v-model="client.gender"/>
                 <label class="label">Женский</label>
             </div>
             <div class="row">
                 <label class="label">Группа клиентов</label>
-                <select class="input-select" multiple required size="1">
+                <select class="input-select" multiple required size="1" v-model="client.group">
                     <option>VIP</option>
                     <option>Проблемные</option>
                     <option>ОМС</option>
@@ -38,7 +38,7 @@
             </div>
             <div class="row">
                 <label class="label">Лечащий врач</label>
-                <select class="input-select">
+                <select class="input-select" v-model="client.doctor">
                     <option>Иванов</option>
                     <option>Захаров</option>
                     <option>Чернышева</option>
@@ -46,41 +46,41 @@
             </div>
             <div class="row">
                 <label class="label">Не отправлять СМС</label>
-                <input type="checkbox" class="input-checkbox" /> 
+                <input type="checkbox" class="input-checkbox" v-model="client.sendSMS"/> 
             </div>
         </div>
         <div class="section">
             <h5 class="section-name">Адрес</h5>
             <div class="row">
                 <label class="label">Индекс</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.adress.index"/>
             </div>
             <div class="row">
                 <label class="label">Страна</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.adress.country"/>
             </div>
             <div class="row">
                 <label class="label">Область</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.adress.region"/>
             </div>
             <div class="row">
                 <label class="label">Город</label>
-                <input type="text" class="input-text" required />
+                <input type="text" class="input-text" required v-model="client.adress.city"/>
             </div>
             <div class="row">
                 <label class="label">Улица</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.adress.street"/>
             </div>
             <div class="row">
                 <label class="label">Дом</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.adress.building"/>
             </div>
         </div>
         <div class="section">
             <h5 class="section-name">Паспорт</h5>
             <div class="row">
                 <label class="label">Тип документа</label>
-                <select class="input-select" required>
+                <select class="input-select" required v-model="client.document.type">
                     <option>Паспорт</option>
                     <option>Свидетельство о рождении</option>
                     <option>Вод. удостоверение</option>
@@ -88,19 +88,19 @@
             </div>
             <div class="row">
                 <label class="label">Серия</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.document.series"/>
             </div>
             <div class="row">
                 <label class="label">Номер</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.document.number"/>
             </div>
             <div class="row">
                 <label class="label">Кем выдан</label>
-                <input type="text" class="input-text" />
+                <input type="text" class="input-text" v-model="client.document.delivered"/>
             </div>
             <div class="row">
                 <label class="label">Дата выдачи</label>
-                <input type="text" class="input-text" required />
+                <input type="text" class="input-text" required v-model="client.document.deliveryDate"/>
             </div>
         </div>
         <button id="button">Создать</button>
@@ -109,6 +109,36 @@
 
 <script>
 export default {
+    data () {
+        return {
+            client: {
+                name: '',
+                surname: '',
+                secondName: '',
+                birthDate: '',
+                phone: '',
+                gender: '',
+                group: '',
+                doctor: '',
+                sendSMS: false,
+                adress: {
+                    index: '',
+                    country: '',
+                    region: '',
+                    city: '',
+                    street: '',
+                    building: ''
+                },
+                document: {
+                    type: '',
+                    series: '',
+                    number: '',
+                    delivered: '',
+                    deliveryDate: ''
+                }
+            }
+        }
+    },
     name: 'Form',
     components: {
 
